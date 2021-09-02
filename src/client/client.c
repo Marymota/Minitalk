55a74eb54d96e434192adb3760534ed7b74667da
marymota <dg.marciamota@gmail.com>
2021-09-02 23:51:57 +0100

    Sends text (including unicode) but is kind of slow...

--- src/client/client.c
+++ src/client/client.c
@@ -1,21 +1,80 @@
 #include "minitalk.h"
 
 /*
-	CLIENT <-> SERVER SETUP
-		##SERVER
-			#1 Create a pid_t variable to store the 'pid' number;
-			#2 Use getpid() to get the process ID;
-			#3 Print the PID so that it can be used when running client: ./client [PID] [string]
+	* kill - send a signal to a process or a group of processes
+	** Function: int kill(pid_t pid, int sig);
+	*** The only signals we can send are SIGUSR1 or SIGUSR2
+	**** SIGUSR1 and SIGUSR2 signals are sent to a process to indicate user-defined conditions.
 
+	CLIENT <-> SERVER SETUP
 		##CLIENT
-			#4 Execute client: ./client [pid] ["message"]
-			#5 EXIT_FAILURE if arguments are not 3 (see #4) 
-			#6 Convert the PID to integer
+			#4	Execute client: ./client [pid] ["message"]
+			#5	EXIT_FAILURE if arguments are not 3 (see #4) 
+			#6	Convert the PID to integer
+
+	ASCII TO BINARY CONVERSION
+		##CLIENT
+			#7	Iterate through the message one character at a time
+				For each character is necessary to send 8 bits (1s and 0s)
+				SIGURS1 will send 1's and SIGUSR2 will send 0's
+
+				-> Letter: 'a' = ASCII: 097 = Binary: 0110 0001
+				-> Hex: 0x80 = Decimal: 128 = Binary: 1000 0000
+				-> 1000 0000 >> bit(0) = 1000 0000;
+				-> 1000 0000 >> bit(1) = 0100 0000;
+				-> 1000 0000 >> bit(2) = 0010 0000; ...
+				-> 1 & 1 = 1; 1 & 0 = 0; 
+			#8	Use kill() to send the corresponding signal to the 
+				to the server by using its process ID.
+			#9 	Use usleep() to suspend execution for microseconds intervals
+				so that signals are not lost.
+			#10 After the message a newline character will also be sent.
+				The "nline" flag indicates that the program must stop after that.
+
 */
+void send_bits(char *message, int pid)
+{
+	int i;
+	int bit;
+	int nline;
+
+	bit = -1;
+	i = 0;
+	nline = 0;
+
+	while (1)
+	{
+		bit = -1;
+		if (!message[i])
+		{
+			nline = 1;
+			message[i] = '\n';
+		}
+		while (++bit < 8)
+		{
+			if (message[i] & (0x80 >> bit))
+			{
+				//ft_putchar_fd('1', 1);
+				if (kill(pid, SIGUSR1) == -1)
+					exit(1);
+			}
+			else 
+			{
+				//ft_putchar_fd('0', 1);
+				if (kill(pid, SIGUSR2) == -1)
+					exit(1);
+			}
+			usleep(200);
+		}
+		if (nline)
+			break ;
+		++i;
+	}
+}
 
 int main(int argc, char *argv[])
 {
-	pid_t pid;
+	int pid;
 
 	if (argc != 3)
 	{
@@ -23,5 +82,6 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	pid = ft_atoi(argv[1]);
-	return (pid);
+	send_bits(argv[2], pid);
+	return (0);
 }
--- src/server/server.c
+++ src/server/server.c
@@ -1,5 +1,3 @@
-#include "minitalk.h"
-
 /*
 	*	The pid_t data type is a signed integer type which is capable of representing
 		a process ID.
@@ -15,13 +13,41 @@
 			#1 Create a pid_t variable to store the 'pid' number;
 			#2 Use getpid() to get the process ID;
 			#3 Print the PID so that it can be used when running client: ./client [PID] [string]
-
-		##CLIENT
-			#4 Execute client: ./client [pid] ["message"]
-			#5 EXIT_FAILURE if arguments are not 3 (see #4) 
-			#6 Convert the PID to integer
+	BINARY TO ASCII CONVERSION
+			#11 The variables used in signal_handler() need to be "static" so that they will preserve
+				their value when the new bit (signal) arrives.
+				The values are reset after we have completes 8 bits that we will the print.
+			#12 Use bitwise operators ^ and | 
+				-> Letter: 'a' = ASCII: 097 = Binary: 0110 0001
+				-> Hex: 0x80 = Decimal: 128 = Binary: 1111 1111
+				Using | (OR) to SIGUSR1 and ^ (exclusive OR) for SIGUSR2
+				we can convert the bits into characters again (including unicode characters);
 */
+#include "minitalk.h"
 
+void signal_handler(int signum)
+{
+	static char c = 0xFF;
+	static int bit = 0;
+
+	if (signum == SIGUSR1)
+	{
+		//ft_putchar_fd('1', 1);
+		c |= 128 >> bit;
+	}
+	else if (signum == SIGUSR2)
+	{
+		//ft_putchar_fd('0', 1);
+		c ^= 128 >> bit;
+	}
+	bit++;
+	if (bit == 8)
+	{
+		ft_putchar_fd(c, 1);
+		bit = 0;
+		c = 0xFF;
+	}
+}
 
 int main(void)
 {
@@ -31,6 +57,8 @@ int main(void)
 	ft_putstr_fd("PID: ", 1);
 	ft_putnbr_fd(pid, 1);
 	ft_putchar_fd('\n', 1);
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
 	while (1)
-		;
+		pause();
 }
